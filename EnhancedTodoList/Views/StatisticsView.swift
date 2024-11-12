640b9e28ba1abcceb8889ffdb0e09d8ed5a00dfc
Lexi Harrop <lexx.harrop@gmail.com>
2024-11-12 11:56:48 -0500

    Added tab view

---
+++ EnhancedTodoList/Views/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  EnhancedTodoList
+//
+//  Created by Lexi on 2024-11-12.
+//
+
+import SwiftUI
+ 
+struct StatisticsView: View {
+    var body: some View {
+        Text("Statistics")
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
